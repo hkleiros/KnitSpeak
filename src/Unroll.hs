d4d55f0eed78be394ab74f42c10bd78afab7a0e8
Kristin <hkleiros@uio.no>
2025-05-30 03:12:55 +0200

    Removed backup and updated Unroll

--- src/Unroll.hs
+++ src/Unroll.hs
@@ -1,7 +1,10 @@
-module Unroll (unroll, unrollLines) where 
-import KSSyntax  (Pattern(..), Instructions, Instruction(..), Course(..), Line(..))
+module Unroll (unroll, unrollLines) where
+import KSSyntax  (Pattern(..), Instructions, Instruction(..), Course(..), Line(..), LineNums)
 import Knittels (Knittel(..))
 import Control.Monad (join)
+import Data.Ord (Ordering)
+import Data.List (sortBy)
+import Data.Function (on)
 
 ------------ Unroll ------------
 unroll :: Pattern -> Pattern
@@ -11,7 +14,7 @@ unroll (Pattern p) = Pattern $ map u p
     u c = c
 
 unrollLines :: Pattern -> Pattern
-unrollLines (Pattern p) = Pattern $ join $ map u p
+unrollLines (Pattern p) = Pattern $ sortBy (compare `on` lineNums) (join $ map u p)
   where
     u (Course l is c) = map (\r -> Course r is c) (allLines l)
     -- TODO: also unroll multiline repeats; need lookup function to do this.
@@ -19,13 +22,21 @@ unrollLines (Pattern p) = Pattern $ join $ map u p
     allLines (Row ln s) = map (\l -> Row [l] s) ln
     allLines (Round ln s) = map (\l -> Round [l] s) ln
 
+lineNums :: Course -> LineNums
+lineNums (Course r _ _) = lines r
+  where lines (Row ln _) = ln
+        lines (Round ln _) = ln
+
+-- compareCourses :: Course -> Course -> Ordering
+-- compareCourses (Course r is cm) (Course r2 is2 cm2) =  compare r r2
+
 unrollInstructions :: Instructions -> Instructions
-unrollInstructions is = is >>= unrollInstruction 
+unrollInstructions is = is >>= unrollInstruction
 
 unrollInstruction :: Instruction -> Instructions
-unrollInstruction (Knittel k) = unrollKnittel k 
+unrollInstruction (Knittel k) = unrollKnittel k
 unrollInstruction (Rep r t) = join $ replicate t (unrollInstructions r)
-unrollInstruction (Loop l e) = [Loop (unrollInstructions l) e] 
+unrollInstruction (Loop l e) = [Loop (unrollInstructions l) e]
 
 
 unrollKnittel :: Knittel -> Instructions
